cc3e60f0404ffa27988b265405f968769dc75411
Manoj Khati <mksea18@gmail.com>
2025-01-19 19:40:35 +0545

    getAll and getById added

--- controller/adminController.js
+++ controller/adminController.js
@@ -28,4 +28,34 @@ const addAdmin = async (req, res) => {
    
 };
 
-module.exports= {addAdmin}; // Export as part of an object
+const getAll= async(req, res) => {
+    try{
+        const admins = await Admin.get();
+        if(!admins){
+            return res.status(400).json({message: "Admin not found"});
+            }
+            return res.status(200).json(admin);
+
+    }catch(err){
+        res.status(500).json({message: "Error finding customer"});
+    }
+};
+
+const getById = async (req, res) => {
+    try{
+        const adminn = Admin.getById(req.params.id);
+    if (!adminn){
+        return res.status(400).json({message: "Admin not found"});
+    } 
+    return res.status(200).json(admin); 
+ }
+ catch(err){
+    res.status(500).json({message: "Error finding Admin"});
+ }
+}
+
+module.exports= {
+    addAdmin,
+    getAll,
+    getById,
+}; // Export as part of an object
